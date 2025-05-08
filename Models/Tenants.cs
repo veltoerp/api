cf80714a1f3a87be58b1da571a95f3d820728ae7
fatihgokce <fatihgokce07@gmail.com>
2025-05-08 15:29:00 +0300

    remove countycode replaced with languagecode

--- Models/Tenants.cs
+++ Models/Tenants.cs
@@ -3,7 +3,7 @@ public class Tenant
 {
     public Guid Id { get; set; } 
     public string Name { get; set; } = string.Empty;
-    public string? CountryCode { get; set; }
+    public string? LanguageCode { get; set; }
     public string? CurrencyCode { get; set; }
     public string TimeZone { get; set; } = "UTC";
     public DateTime CreatedAt { get; set; }
